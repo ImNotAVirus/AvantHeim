e3291368d118cc128f78d7db4e6707d87a12d91e
Fizo55 <hugomacees56000@gmail.com>
2021-08-28 00:22:34 +0200

    Fix buy packet view

--- apps/channel_endpoint/lib/channel_endpoint/endpoint/views/shop_views.ex
+++ apps/channel_endpoint/lib/channel_endpoint/endpoint/views/shop_views.ex
@@ -6,14 +6,12 @@ defmodule ChannelEndpoint.Endpoint.ShopViews do
   alias ChannelEndpoint.Endpoint.ShopPackets.Buy
   alias CachingService.Player.Character
 
-  import ChannelEndpoint.Endpoint.ShopPackets.BuyEnums, only: [shop_type: 1]
-
   ## Public API
 
   @spec render(atom, any) :: any
   def render(:buy, %{entity: %Character{id: id}, slot: slot, amount: amount}) do
     %Buy{
-      shop_type: shop_type(:character_shop),
+      shop_type: :character_shop,
       entity_id: id,
       slot: slot,
       amount: amount
