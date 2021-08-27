3d9a2c91a58867f73390ef57b00e896d6bd99e0e
Fizo55 <hugomacees56000@gmail.com>
2021-08-27 22:02:41 +0200

    Add buy packet (#1)
    * Add buy packet
    
    * Remove useless alias
    
    * EntityId can't be equal to 0
    
    * Fix enum name

---
+++ apps/channel_endpoint/lib/channel_endpoint/endpoint/views/shop_views.ex
@@ -0,0 +1,19 @@
+defmodule ChannelEndpoint.Endpoint.ShopViews do
+  @moduledoc """
+  TODO: Documentation
+  """
+
+  alias ChannelEndpoint.Endpoint.ShopPackets.Buy
+
+  ## Public API
+
+  @spec render(atom, any) :: any
+  def render(:buy, %{shop_type: shop_type, entity_id: entity_id, slot: slot, amount: amount}) do
+    %Buy{
+      shop_type: shop_type,
+      entity_id: entity_id,
+      slot: slot,
+      amount: amount
+    }
+  end
+end
