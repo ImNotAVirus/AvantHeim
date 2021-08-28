1c726743ae10e9cd03a11a3935677363f15f631a
Fizo55 <hugomacees56000@gmail.com>
2021-08-28 20:46:02 +0200

    Add callback

--- apps/channel_endpoint/lib/channel_endpoint/endpoint/commands/gold.ex
+++ apps/channel_endpoint/lib/channel_endpoint/endpoint/commands/gold.ex
@@ -6,6 +6,7 @@ defmodule ChannelEndpoint.Endpoint.GoldCommand do
   alias Core.Socket
   alias ChannelEndpoint.Endpoint.ChatViews
   alias ChannelEndpoint.Endpoint.EntityInteractions
+  alias CachingService.Player.Character
 
   ## Public API
 
@@ -25,13 +26,16 @@ defmodule ChannelEndpoint.Endpoint.GoldCommand do
   # DarkyZ have now 0 golds
   #
   # > $gold set 0 to UnknowPlayer
-  # UnknowPlayer is no logged
+  # UnknowPlayer is not logged
   #
   # > $gold set 3 to DarkyZ
   # DarkyZ have now 3 golds
   #
   # > $gold get
-  # Current gold: 0 golds
+  # Current DarkyZ gold: 0 golds
+  #
+  # > $gold get from DarkyZ
+  # > Current DarkyZ gold: #{target.gold} golds
 
   @spec handle_command(String.t(), [String.t()], Socket.t()) :: {:cont, Socket.t()}
   def handle_command("$gold", args, socket) do
@@ -40,19 +44,16 @@ defmodule ChannelEndpoint.Endpoint.GoldCommand do
 
     case args do
       ["get"] ->
-        send_message(socket, character, "Current gold: #{character.gold} golds", :special_green)
+        get_gold(socket, character, args, character)
+
+      ["get", "from", name] ->
+        apply_on_character(socket, character, args, name, &get_gold/4)
 
       ["set", _] = args ->
         set_gold(socket, character, args, character)
 
       ["set", _, "to", name] = args ->
-        case CachingService.get_character_by_name(name) do
-          {:ok, nil} ->
-            send_message(socket, character, "#{name} is not logged", :special_red)
-
-          {:ok, target} ->
-            set_gold(socket, character, args, target)
-        end
+        apply_on_character(socket, character, args, name, &set_gold/4)
 
       args ->
         send_message(socket, character, usage(args), :special_red)
@@ -63,10 +64,26 @@ defmodule ChannelEndpoint.Endpoint.GoldCommand do
 
   ## Private functions
 
-  defp usage(["get" | _]), do: "Usage: $gold get"
-  defp usage(["set" | _]), do: "Usage: $gold set value:integer:0-2_000_000_000"
-  defp usage(_), do: "Usage: $gold <get|set> [value:integer:0-2_000_000_000]"
+  defp usage(["get" | _]), do: "Usage: $gold <get>"
+  defp usage(["set" | _]), do: "Usage: $gold <set> [value:integer:0-2_000_000_000] [to] [player_name:string]"
+  defp usage(_), do: "Usage: $gold <get|set> [value:integer:0-2_000_000_000] [to] [player_name:string] [from] [player_name:string]"
 
+  @spec apply_on_character(Socket.t(), Character.t(), [String.t()], String.t(), any) :: {:ok, Character.t()}
+  defp apply_on_character(socket, character, args, name, callback) do
+    case CachingService.get_character_by_name(name) do
+      {:ok, nil} ->
+        send_message(socket, character, "#{name} is not logged", :special_red)
+
+      {:ok, target} ->
+        callback.(socket, character, args, target)
+    end
+  end
+
+  defp get_gold(socket, character, [_, str_val | _] = args, target) do
+    send_message(socket, character, "Current #{target.name} gold: #{target.gold} golds", :special_green)
+  end
+
+  @spec set_gold(Socket.t(), Character.t(), [String.t()], Character.t()) :: :ok
   defp set_gold(socket, character, [_, str_val | _] = args, target) do
     case Integer.parse(str_val) do
       {value, ""} ->
