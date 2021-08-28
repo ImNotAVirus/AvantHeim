d77783728276086e8e44011f1bd3e0e395061b6d
Fizo55 <hugomacees56000@gmail.com>
2021-08-28 20:15:09 +0200

    Add set gold to | Update example

--- apps/channel_endpoint/lib/channel_endpoint/endpoint/commands/gold.ex
+++ apps/channel_endpoint/lib/channel_endpoint/endpoint/commands/gold.ex
@@ -16,13 +16,19 @@ defmodule ChannelEndpoint.Endpoint.GoldCommand do
   # Invalid value 'test'
   #
   # > $gold set 2_000_000_001
-  # You have now 2_000_000_000 golds
+  # DarkyZ have now 2_000_000_000 golds
   #
   # > $gold set 666
-  # You have now 666 golds
+  # DarkyZ have now 666 golds
   #
   # > $gold set -1
-  # You have now 0 golds
+  # DarkyZ have now 0 golds
+  #
+  # > $gold set 0 to UnknowPlayer
+  # UnknowPlayer is no logged
+  #
+  # > $gold set 3 to DarkyZ
+  # DarkyZ have now 3 golds
   #
   # > $gold get
   # Current gold: 0 golds
@@ -36,15 +42,16 @@ defmodule ChannelEndpoint.Endpoint.GoldCommand do
       ["get"] ->
         send_message(socket, character, "Current gold: #{character.gold} golds", :special_green)
 
-      ["set", str_val] = args ->
-        case Integer.parse(str_val) do
-          {value, ""} ->
-            {:ok, new_char} = EntityInteractions.set_player_gold(character, value)
-            send_message(socket, new_char, "You have now #{value} golds", :special_green)
+      ["set", _] = args ->
+        set_gold(socket, character, args, character)
 
-          _ ->
-            send_message(socket, character, "Invalid value '#{str_val}'", :special_red)
-            send_message(socket, character, usage(args), :special_red)
+      ["set", _, "to", name] = args ->
+        case CachingService.get_character_by_name(name) do
+          {:ok, nil} ->
+            send_message(socket, character, "#{name} is not logged", :special_red)
+
+          {:ok, target} ->
+            set_gold(socket, character, args, target)
         end
 
       args ->
@@ -59,6 +66,18 @@ defmodule ChannelEndpoint.Endpoint.GoldCommand do
   defp usage(["get" | _]), do: "Usage: $gold get"
   defp usage(["set" | _]), do: "Usage: $gold set value:integer:0-2_000_000_000"
   defp usage(_), do: "Usage: $gold <get|set> [value:integer:0-2_000_000_000]"
+
+  defp set_gold(socket, character, [_, str_val | _] = args, target) do
+    case Integer.parse(str_val) do
+      {value, ""} ->
+        {:ok, new_char} = EntityInteractions.set_player_gold(target, value)
+        send_message(socket, new_char, "#{new_char.name} have now #{new_char.gold} golds", :special_green)
+
+      _ ->
+        send_message(socket, character, "Invalid value '#{str_val}'", :special_red)
+        send_message(socket, character, usage(args), :special_red)
+    end
+  end
 
   defp send_message(socket, character, msg, color) do
     render = ChatViews.render(:say, %{entity: character, color: color, message: msg})
