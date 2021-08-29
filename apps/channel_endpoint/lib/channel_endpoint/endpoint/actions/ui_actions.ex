1386373ac26f6ed7e0feef5864ddafe1468d7416
Fizo55 <hugomacees56000@gmail.com>
2021-08-29 21:26:54 +0200

    Remove case in case

--- apps/channel_endpoint/lib/channel_endpoint/endpoint/actions/ui_actions.ex
+++ apps/channel_endpoint/lib/channel_endpoint/endpoint/actions/ui_actions.ex
@@ -20,18 +20,18 @@ defmodule ChannelEndpoint.Endpoint.UIActions do
     %{guri_data: guri_data, entity_id: entity_id, entity_type: entity_type} = params
     entity = CachingService.get_entity_by_id(entity_type, entity_id)
 
-    case entity do
-      {:ok, character} ->
-        case guri_data do
-          x when x >= 973 and x <= 999 ->
-            EntityInteractions.show_effect(character, guri_data + @emoji_offset)
+    case {entity, guri_data} do
+      {{:ok, e}, g} when g >= 973 and g <= 999 ->
+        EntityInteractions.show_effect(e, guri_data + @emoji_offset)
 
-          1000 ->
-            EntityInteractions.show_effect(character, @rainbow_vomit_vnum)
+      {{:ok, e}, 1000} ->
+        EntityInteractions.show_effect(e, @rainbow_vomit_vnum)
 
-          x ->
-            Socket.send(socket, UIViews.render(:info, %{message: "UNAUTHORIZED_EMOTICON"}))
-        end
+      {{:ok, _e}, _} ->
+        Socket.send(socket, UIViews.render(:info, %{message: "UNAUTHORIZED_EMOTICON"}))
+
+      _ ->
+        :ok
     end
 
     {:cont, socket}
