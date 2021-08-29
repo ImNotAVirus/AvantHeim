4cd0162703addddc755f246e2e7fdbdcd397e453
Fizo55 <hugomacees56000@gmail.com>
2021-08-29 21:28:42 +0200

    Add player emotes (#8)
    * Add player emotes
    
    * Add info message
    
    * Cleanup
    
    * Add socket send on UIViews render
    
    * Add entity_type check
    
    * Remove case in case

---
+++ apps/channel_endpoint/lib/channel_endpoint/endpoint/packets/ui_packets/guri_enums.ex
@@ -0,0 +1,15 @@
+defmodule ChannelEndpoint.Endpoint.UIPackets.Guri do
+  @moduledoc """
+  TODO: Documentation.
+  """
+
+  import SimpleEnum, only: [defenum: 2]
+
+  defenum :guri_type,
+    emoji: 10,
+    scene_req_act1: 40,
+    scene_req_act2: 41,
+    scene_req_act3: 42,
+    scene_req_act4: 43,
+    scene_req_act5: 44
+end
