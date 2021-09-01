3cdfeb868f2ba44f4923b8a3abeb034b35f7388b
ImNotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2021-09-01 17:06:59 +0200

    :sparkles: Simple A star algo

--- apps/algorithms/lib/pathfinding.ex
+++ apps/algorithms/lib/pathfinding.ex
@@ -1,6 +1,8 @@
 defmodule Algorithms.Pathfinding do
   use Rustler, otp_app: :algorithms, crate: "pathfinding_nif"
 
-  # When your NIF is loaded, it will override this function.
-  def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+  # Algorithms.Pathfinding.astar(<<0,0,0,0,0,0,1,0,1,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0>>,5,5,{0,0},{4,4})
+  @type coord :: {non_neg_integer, non_neg_integer}
+  @spec astar(binary, pos_integer, pos_integer, coord, coord) :: {:ok, [coord]} | {:error, atom}
+  def astar(_map, _width, _height, _start, _end), do: :erlang.nif_error(:nif_not_loaded)
 end
