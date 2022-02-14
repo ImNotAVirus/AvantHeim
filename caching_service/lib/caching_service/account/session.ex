e8e9acce48b130d513699b6b6d9f9b240e1747e9
ImNotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2022-02-14 15:53:56 +0100

    :sparkles: Add Session caching struct

---
+++ caching_service/lib/caching_service/account/session.ex
@@ -0,0 +1,72 @@
+defmodule CachingService.Account.Session do
+  @moduledoc """
+  TODO: Documentation
+  """
+
+  alias __MODULE__
+
+  @required_attributes [:username, :password, :account_id, :encryption_key]
+  @virtual_attributes %{expire: nil, state: :authenticated}
+  @states [:authenticated, :in_lobby, :in_game, :saving]
+  @default_ttl Application.get_env(:caching_service, :session_ttl, 120)
+
+  use Memento.Table,
+    attributes: @required_attributes ++ Map.keys(@virtual_attributes),
+    index: [:encryption_key],
+    type: :ordered_set
+
+  @type state :: :authenticated | :in_lobby | :in_game | :saving
+
+  @type t :: %Session{
+          username: String.t(),
+          password: String.t(),
+          account_id: pos_integer,
+          encryption_key: pos_integer,
+          expire: timeout,
+          state: state
+        }
+
+  ## Public API
+
+  defguard is_logged(s) when is_struct(s, Session) and s.state in [:in_lobby, :in_game, :saving]
+
+  @spec new(map) :: t()
+  def new(attrs) do
+    default = %{expire: ttl_to_expire(@default_ttl)}
+
+    attrs
+    |> extract_attributes!(@required_attributes)
+    |> Map.merge(@virtual_attributes)
+    |> Map.merge(default)
+    |> then(&struct!(Session, &1))
+  end
+
+  @spec set_ttl(t(), timeout) :: t()
+  def set_ttl(%Session{} = session, ttl) do
+    %Session{session | expire: ttl_to_expire(ttl)}
+  end
+
+  @spec set_state(t(), atom) :: t()
+  def set_state(%Session{} = session, state) when state in @states do
+    %Session{session | state: state}
+  end
+
+  ## Internal helpers
+
+  @spec ttl_to_expire(timeout) :: timeout
+  def ttl_to_expire(ttl) do
+    case ttl do
+      :infinity -> :infinity
+      _ -> System.monotonic_time(:millisecond) + ttl
+    end
+  end
+
+  ## Private functions
+
+  defp extract_attributes!(attrs, required_attributes) do
+    case required_attributes -- Map.keys(attrs) do
+      [] -> Map.take(attrs, required_attributes)
+      missing -> raise ArgumentError, "missing attributes: #{inspect(missing)}"
+    end
+  end
+end
---
+++ caching_service/test/support/account_case.ex
@@ -0,0 +1,50 @@
+defmodule AccountCase do
+  use ExUnit.CaseTemplate
+
+  alias CachingService.Account.Session
+
+  setup do
+    # This will run before each test that uses this case
+    :ok
+  end
+
+  using do
+    quote do
+      import AccountCase,
+        only: [
+          session_mock: 0,
+          session_mock: 1,
+          session_attrs_mock: 0,
+          session_attrs_mock: 1
+        ]
+    end
+  end
+
+  ## Mocks
+
+  def session_mock(attrs \\ %{}) do
+    attrs |> session_attrs_mock() |> Session.new()
+  end
+
+  def session_attrs_mock(attrs \\ %{}) do
+    Map.merge(
+      %{
+        username: random_string(),
+        password: "password:#{random_string()}",
+        account_id: random_integer(),
+        encryption_key: random_integer()
+      },
+      attrs
+    )
+  end
+
+  ## Helpers
+
+  defp random_integer() do
+    System.unique_integer([:positive])
+  end
+
+  defp random_string() do
+    for _ <- 1..10, into: "", do: <<Enum.random('0123456789abcdef')>>
+  end
+end
