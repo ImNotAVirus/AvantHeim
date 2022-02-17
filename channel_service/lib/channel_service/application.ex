900495f080865ca50b0fa1de28a433ab7f8d95f5
ImNotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2022-02-17 18:26:50 +0100

    :recycle: Use SessionRegistry module

--- channel_service/lib/channel_service/application.ex
+++ channel_service/lib/channel_service/application.ex
@@ -5,11 +5,16 @@ defmodule ChannelService.Application do
 
   @impl true
   def start(_type, _args) do
+    # topologies = Application.get_env(:libcluster, :topologies, [])
+
     children = [
+      # {Cluster.Supervisor, [topologies, [name: LoginService.ClusterSupervisor]]},
+      # {ElvenCaching.MnesiaClusterManager, []},
+      {ElvenCaching.SessionRegistry, []},
       {ChannelService.Endpoint, name: ChannelService.Endpoint}
     ]
 
-    opts = [strategy: :one_for_one, name: ChannelService.Supervisor]
+    opts = [strategy: :one_for_all, name: ChannelService.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
