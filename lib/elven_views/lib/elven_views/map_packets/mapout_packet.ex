45c8e7a9beb37cc9a275e6cad20ebcf8964d485f
Worldie1 <AxeTrix@hotmail.com>
2022-02-19 03:08:34 +0100

    create mapout packet

---
+++ lib/elven_views/lib/elven_views/map_packets/mapout_packet.ex
@@ -0,0 +1,12 @@
+defmodule ElvenViews.MapPackets.MapOutPacket do
+  @moduledoc """
+  TODO: Documentation.
+  """
+
+  use ElvenViews.SerializablePacket
+
+  ## Packet definition
+
+  defpacket "mapout" do
+  end
+end
