d7be19768cbd68ae4361da4b3c686c011052879e
ImNotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2022-02-19 13:42:41 +0100

    :ambulance: Fix c_info packet test

--- lib/elven_views/test/elven_views/player_packets/c_info_packet_test.exs
+++ lib/elven_views/test/elven_views/player_packets/c_info_packet_test.exs
@@ -10,14 +10,14 @@ defmodule ElvenViews.PlayerPackets.CInfoPacketTest do
       packet = structure_to_iolist(c_info_mock())
 
       assert is_list(packet)
-      assert length(packet) == 19
+      assert length(packet) == 20
       assert packet_index(packet, 0) == "c_info"
-      assert packet_index(packet, 1) == "4615"
-      assert packet_index(packet, 2) == "DarkyZ"
+      assert packet_index(packet, 1) == "DarkyZ"
+      assert packet_index(packet, 2) == "-"
       assert packet_index(packet, 3) == "221"
-      assert packet_index(packet, 4) == "331"
-      assert packet_index(packet, 5) == "2"
-      assert packet_index(packet, 6) == "AvantHeim"
+      assert packet_index(packet, 4) == "331.916"
+      assert packet_index(packet, 5) == "AvantHeim"
+      assert packet_index(packet, 6) == "4615"
       assert packet_index(packet, 7) == "10"
       assert packet_index(packet, 8) == "0"
       assert packet_index(packet, 9) == "3"
@@ -29,32 +29,52 @@ defmodule ElvenViews.PlayerPackets.CInfoPacketTest do
       assert packet_index(packet, 15) == "0"
       assert packet_index(packet, 16) == "12"
       assert packet_index(packet, 17) == "15"
-      assert packet_index(packet, 18) == "1"
+      assert packet_index(packet, 18) == "16"
+      assert packet_index(packet, 19) == "1"
+    end
+
+    test "can serialize when no family is given" do
+      family_attrs = %{
+        family_id: -1,
+        family_rank: :member,
+        family_name: nil,
+        family_level: 0
+      }
+
+      packet = structure_to_iolist(c_info_mock(family_attrs))
+
+      assert packet_index(packet, 4) == "-1"
+      assert packet_index(packet, 5) == "-"
+      assert packet_index(packet, 16) == "0"
     end
   end
 
   ## Helpers
 
-  defp c_info_mock() do
-    %CInfoPacket{
-      character_id: 4615,
-      name: "DarkyZ",
-      group_id: 221,
-      family_id: 331,
-      family_rank: :deputy,
-      family_name: "AvantHeim",
-      name_color_id: 10,
-      gender: :male,
-      hair_style: :hair_style_d,
-      hair_color: :brown,
-      class: :martial_artist,
-      reputation_icon_id: 80,
-      compliment: 100,
-      morph: 1024,
-      is_invisible: false,
-      family_level: 12,
-      morph_upgrade: 15,
-      is_arena_winner: true
-    }
+  defp c_info_mock(attrs \\ %{}) do
+    Map.merge(
+      %CInfoPacket{
+        character_id: 4615,
+        name: "DarkyZ",
+        group_id: 221,
+        family_id: 331,
+        family_rank: :deputy,
+        family_name: "AvantHeim",
+        name_color_id: 10,
+        gender: :male,
+        hair_style: :hair_style_d,
+        hair_color: :brown,
+        class: :martial_artist,
+        reputation_icon_id: 80,
+        compliment: 100,
+        morph: 1024,
+        is_invisible: false,
+        family_level: 12,
+        morph_upgrade: 15,
+        morph_design: 16,
+        is_arena_winner: true
+      },
+      attrs
+    )
   end
 end
