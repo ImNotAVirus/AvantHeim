7f0c710832886b7d82e3c260b177179815dae22e
Worldie1 <AxeTrix@hotmail.com>
2022-02-21 12:51:49 +0100

    add cancel packet

---
+++ lib/elven_views/test/elven_views/ui_packets/cancel_packet_test.exs
@@ -0,0 +1,28 @@
+defmodule ElvenViews.UIPackets.CancelPacketTest do
+  use PacketCase, async: true
+
+  alias ElvenViews.UIPackets.CancelPacket
+
+  ## Tests
+
+  describe "serialize/2" do
+    test "can serialize a packet structure" do
+      packet = structure_to_iolist(cancel_mock())
+
+      assert is_list(packet)
+      assert length(packet) == 4
+      assert packet_index(packet, 0) == "cancel"
+      assert packet_index(packet, 1) == "0"
+      assert packet_index(packet, 2) == "0"
+      assert packet_index(packet, 3) == "-1"
+    end
+  end
+
+  ## Helpers
+
+  defp cancel_mock() do
+    %CancelPacket{
+      cancel_type: :auto_attack
+    }
+  end
+end
