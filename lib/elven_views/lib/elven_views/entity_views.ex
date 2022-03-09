36469583bd4d91b061e43427e1dd3f0f4b615a1f
ImNotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2022-03-09 16:33:49 +0100

    :recycle: Refacto some views

--- lib/elven_views/lib/elven_views/entity_views.ex
+++ lib/elven_views/lib/elven_views/entity_views.ex
@@ -23,21 +23,18 @@ defmodule ElvenViews.EntityViews do
   ## Public API
 
   @impl true
-  def render(:st, args) do
-    entity = required_param(args, :entity)
-    buffs = optional_param(args, :buffs, [])
+  def render(:c_mode, args) do
+    character = required_param(args, :character)
 
-    %StPacket{
-      entity_type: Entity.type(entity),
-      entity_id: Entity.id(entity),
-      level: LevelableEntity.level(entity),
-      hero_level: LevelableEntity.hero_level(entity),
-      hp: BattleEntity.hp(entity),
-      hp_percent: BattleEntityHelper.hp_percent(entity),
-      mp: BattleEntity.mp(entity),
-      mp_percent: BattleEntityHelper.mp_percent(entity),
-      # TODO: Buff is a System. Not sure how to do it currently
-      buffs: buffs
+    %CModePacket{
+      entity_type: :character,
+      entity_id: character.id,
+      morph: FakeData.morph(character_id: character.id),
+      morph_upgrade: FakeData.morph_upgrade(character_id: character.id),
+      morph_design: FakeData.morph_design(character_id: character.id),
+      is_arena_winner: FakeData.is_arena_winner(character_id: character.id),
+      size: FakeData.size(character_id: character.id),
+      item_morph: FakeData.item_morph(character_id: character.id)
     }
   end
 
@@ -64,6 +61,16 @@ defmodule ElvenViews.EntityViews do
     }
   end
 
+  def render(:dir, args) do
+    entity = required_param(args, :entity)
+
+    %DirPacket{
+      entity_type: Entity.type(entity),
+      entity_id: Entity.id(entity),
+      direction: MapEntity.direction(entity)
+    }
+  end
+
   def render(:eff, args) do
     entity = required_param(args, :entity)
     value = required_param(args, :value)
@@ -75,28 +82,21 @@ defmodule ElvenViews.EntityViews do
     }
   end
 
-  def render(:dir, args) do
+  def render(:st, args) do
     entity = required_param(args, :entity)
+    buffs = optional_param(args, :buffs, [])
 
-    %DirPacket{
+    %StPacket{
       entity_type: Entity.type(entity),
       entity_id: Entity.id(entity),
-      direction: MapEntity.direction(entity)
-    }
-  end
-
-  def render(:c_mode, args) do
-    character = required_param(args, :character)
-
-    %CModePacket{
-      entity_type: :character,
-      entity_id: character.id,
-      morph: FakeData.morph(character_id: character.id),
-      morph_upgrade: FakeData.morph_upgrade(character_id: character.id),
-      morph_design: FakeData.morph_design(character_id: character.id),
-      is_arena_winner: FakeData.is_arena_winner(character_id: character.id),
-      size: FakeData.size(character_id: character.id),
-      item_morph: FakeData.item_morph(character_id: character.id)
+      level: LevelableEntity.level(entity),
+      hero_level: LevelableEntity.hero_level(entity),
+      hp: BattleEntity.hp(entity),
+      hp_percent: BattleEntityHelper.hp_percent(entity),
+      mp: BattleEntity.mp(entity),
+      mp_percent: BattleEntityHelper.mp_percent(entity),
+      # TODO: Buff is a System. Not sure how to do it currently
+      buffs: buffs
     }
   end
 end
