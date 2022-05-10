cbe45b89628725b2bf78766bc553f9eb3603c22f
Worldito <AxeTrix@hotmail.com>
2022-05-10 11:39:43 +0200

    [ADD] MP Algorithms and its tests

--- lib/elven_algorithms/lib/elven_algorithms/battle_algorithms.ex
+++ lib/elven_algorithms/lib/elven_algorithms/battle_algorithms.ex
@@ -3,6 +3,8 @@ defmodule ElvenAlgorithms.BattleAlgorithms do
   TODO: Documentation
   """
 
+  import ElvenAlgorithms
+
   @max_level 99
 
   ## HP Max Algorithm
@@ -21,6 +23,30 @@ defmodule ElvenAlgorithms.BattleAlgorithms do
 
     def hp_max(unquote(class), unquote(level)) do
       unquote(hp)
+    end
+  end
+
+  ## MP Max Algorithm
+
+  @mp_constant %{
+    adventurer: 0,
+    swordman: 0,
+    archer: 1,
+    magician: 8,
+    martial_artist: 2
+  }
+
+  for level <- 1..@max_level, {class, multiplier} <- @mp_constant do
+    mpx = level + floor((level - 1) * multiplier / 10)
+
+    mp =
+      floor(9.25 * mpx + 50.75) +
+        (mpx - 2) / 4 * 2 * (modulus(mpx - 2, 4) + 1 + (mpx - 6) / 4 * 2)
+
+    mp_trunc = trunc(mp)
+
+    def mp_max(unquote(class), unquote(level)) do
+      unquote(mp_trunc)
     end
   end
 end
