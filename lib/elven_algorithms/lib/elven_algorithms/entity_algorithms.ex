f7378a28c39661639a31b2488207aca544929d31
Worldito <AxeTrix@hotmail.com>
2022-05-22 22:32:51 +0200

    delete entity_algorithms files

--- lib/elven_algorithms/lib/elven_algorithms/entity_algorithms.ex
+++
@@ -1,5 +0,0 @@
-defmodule ElvenAlgorithms.EntityAlgorithms do
-  @moduledoc """
-  TODO: Documentation
-  """
-end
--- lib/elven_algorithms/test/elven_algorithms/entity_algorithms_test.exs
+++
@@ -1,7 +0,0 @@
-defmodule ElvenAlgorithms.EntityAlgorithmsTest do
-  use ExUnit.Case
-
-  alias ElvenAlgorithms.EntityAlgorithms
-
-  ## Tests
-end
