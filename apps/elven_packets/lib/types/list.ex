63d2b0582d1516f8b1d06004e35ba080f108664f
DarkyZ aka NotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2023-08-10 14:50:38 +0200

    :sparkles: Add List type

---
+++ apps/elven_packets/lib/types/list.ex
@@ -0,0 +1,55 @@
+defmodule ElvenPackets.Types.NsList do
+  @moduledoc """
+  TODO: ElvenPackets.Types.NsList
+  """
+
+  use ElvenGard.Network.Type
+
+  alias ElvenPackets.Types.NsString
+
+  @type t :: list()
+
+  ## Behaviour impls
+
+  @impl true
+  @spec decode(binary(), Keyword.t()) :: {t(), binary()}
+  def decode(data, opts) when is_binary(data) do
+    type = Keyword.get(opts, :type)
+    joiner = Keyword.fetch!(opts, :joiner)
+
+    {encoded, rest} =
+      case joiner do
+        " " -> {data, ""}
+        _ -> NsString.decode(data)
+      end
+
+    encoded
+    |> String.split(joiner)
+    |> do_decode(type)
+    |> then(&{&1, rest})
+  end
+
+  @impl true
+  @spec encode(t(), Keyword.t()) :: iolist()
+  def encode(list, opts) when is_list(list) do
+    type = Keyword.get(opts, :type)
+    joiner = Keyword.fetch!(opts, :joiner)
+
+    case {type, joiner} do
+      {nil, " "} -> list
+      {_, " "} -> Enum.map(list, &type.encode/1)
+      {nil, _} -> Enum.join(list, joiner)
+      _ -> list |> Enum.map(&type.encode/1) |> Enum.join(joiner)
+    end
+  end
+
+  ## Helpers
+
+  defp do_decode(encoded_list, nil), do: encoded_list
+  defp do_decode(encoded_list, type), do: Enum.map(encoded_list, &do_decode_type(&1, type))
+
+  defp do_decode_type(obj, type) do
+    {result, ""} = type.decode(obj)
+    result
+  end
+end
