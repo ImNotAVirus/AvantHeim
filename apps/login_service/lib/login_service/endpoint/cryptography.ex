56c79df4ffffcfa72e9d205b3458d0a0043261c9
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-08-14 13:44:47 +0000

    Fix login next

--- apps/login_service/lib/login_service/endpoint/cryptography.ex
+++ apps/login_service/lib/login_service/endpoint/cryptography.ex
@@ -5,9 +5,28 @@ defmodule LoginService.Endpoint.Cryptography do
 
   import Bitwise, only: [band: 2, bxor: 2]
 
+  @doc """
+  Split a login packet into the next packet and the rest of the packet.
+
+  ## Examples
+
+      iex> LoginService.Endpoint.Cryptography.next(<<156, 187, 159, 2, 5, 3, 5, 242, 255, 4, 1, 6, 2, 255, 10, 242, 177, 242, 5, 145, 149, 4, 0, 5, 4, 4, 5, 148, 255, 149, 2, 144, 150, 2, 145, 2, 4, 5, 149, 150, 2, 3, 145, 6, 1, 9, 10, 9, 149, 6, 2, 0, 5, 144, 3, 9, 150, 1, 255, 9, 255, 2, 145, 0, 145, 10, 143, 5, 3, 150, 4, 144, 6, 255, 0, 5, 0, 0, 4, 3, 2, 3, 150, 9, 5, 4, 145, 2, 10, 0, 150, 1, 149, 9, 1, 144, 6, 150, 9, 4, 145, 3, 9, 255, 5, 4, 0, 150, 148, 9, 10, 148, 150, 2, 255, 143, 9, 150, 143, 148, 3, 6, 255, 143, 9, 143, 3, 144, 6, 149, 255, 2, 5, 5, 150, 6, 148, 9, 148, 2, 9, 144, 145, 2, 1, 5, 242, 2, 2, 255, 9, 149, 255, 150, 143, 215, 2, 252, 9, 252, 255, 252, 255, 2, 3, 1, 242, 2, 242, 143, 3, 150, 0, 5, 2, 255, 144, 150, 0, 5, 3, 148, 5, 144, 145, 149, 2, 10, 3, 2, 148, 6, 2, 143, 0, 150, 145, 255, 4, 4, 4, 216, 54, 63>>)
+      {<<156, 187, 159, 2, 5, 3, 5, 242, 255, 4, 1, 6, 2, 255, 10, 242, 177, 242, 5, 145, 149, 4, 0, 5, 4, 4, 5, 148, 255, 149, 2, 144, 150, 2, 145, 2, 4, 5, 149, 150, 2, 3, 145, 6, 1, 9, 10, 9, 149, 6, 2, 0, 5, 144, 3, 9, 150, 1, 255, 9, 255, 2, 145, 0, 145, 10, 143, 5, 3, 150, 4, 144, 6, 255, 0, 5, 0, 0, 4, 3, 2, 3, 150, 9, 5, 4, 145, 2, 10, 0, 150, 1, 149, 9, 1, 144, 6, 150, 9, 4, 145, 3, 9, 255, 5, 4, 0, 150, 148, 9, 10, 148, 150, 2, 255, 143, 9, 150, 143, 148, 3, 6, 255, 143, 9, 143, 3, 144, 6, 149, 255, 2, 5, 5, 150, 6, 148, 9, 148, 2, 9, 144, 145, 2, 1, 5, 242, 2, 2, 255, 9, 149, 255, 150, 143, 215, 2, 252, 9, 252, 255, 252, 255, 2, 3, 1, 242, 2, 242, 143, 3, 150, 0, 5, 2, 255, 144, 150, 0, 5, 3, 148, 5, 144, 145, 149, 2, 10, 3, 2, 148, 6, 2, 143, 0, 150, 145, 255, 4, 4, 4>>, <<54, 63>>}
+
+  """
   @spec next(binary(), binary()) :: {binary(), binary()}
+  def next(packet, acc \\ <<>>)
+
+  def next(<<>>, acc) do
+    {acc, <<>>}
+  end
+
   def next(<<0xD8, rest::binary>>, acc) do
-    do_next(rest, acc)
+    {acc, rest}
+  end
+
+  def next(<<c, rest::binary>>, acc) do
+    next(rest, acc <> <<c>>)
   end
 
   @doc """
@@ -22,7 +41,7 @@ defmodule LoginService.Endpoint.Cryptography do
   def encrypt(packet) do
     data =
       for <<c <- packet>>, into: <<>> do
-        <<band(c + 15, 0xFF)::size(8)>>
+        <<band(c + 15, 0xFF)::8>>
       end
 
     <<data::binary, 0x19>>
@@ -37,8 +56,19 @@ defmodule LoginService.Endpoint.Cryptography do
       "NoS0575 3614038 a 5AE625665F3E0BD0A065ED07A41989E4025B79D13930A2A8C57D6B4325226707D956A082D1E91B4D96A793562DF98FD03C9DCF743C9C7B4E3055D4F9F09BA015 0039E3DC\x0B0.9.3.3071 0 C7D2503BD257F5BAE0870F40C2DA3666"
   """
   @spec decrypt(binary, map) :: binary()
-  def decrypt(binary, _assigns) do
-    for <<c <- binary>>, into: <<>>, do: do_decrypt(c)
+  def decrypt(<<>>, _assigns) do
+    <<>>
+  end
+
+  def decrypt(<<c>>, _assigns) do
+    case c do
+      c when c > 0x0E -> <<bxor(c - 0x0F, 0xC3)::utf8>>
+      c -> <<bxor(0xFF - (0x0E - c), 0xC3)::utf8>>
+    end
+  end
+
+  def decrypt(binary, assigns) do
+    for <<c <- binary>>, into: <<>>, do: decrypt(<<c>>, assigns)
   end
 
   @doc """
@@ -67,25 +97,5 @@ defmodule LoginService.Endpoint.Cryptography do
     |> Stream.map(&Enum.join/1)
     |> Enum.map(&String.to_integer(&1, 16))
     |> to_string()
-  end
-
-  #
-  # Private functions
-  #
-
-  @spec do_decrypt(byte) :: binary
-  defp do_decrypt(c) do
-    case c do
-      c when c > 0x0E -> <<bxor(c - 0x0F, 0xC3)::utf8>>
-      c -> <<bxor(0xFF - (0x0E - c), 0xC3)::utf8>>
-    end
-  end
-
-  defp do_next(<<0xD8, rest::binary>>, acc) do
-    do_next(rest, acc <> <<0xD8>>)
-  end
-
-  defp do_next(<<0xD9, rest::binary>>, acc) do
-    {acc, rest}
   end
 end
