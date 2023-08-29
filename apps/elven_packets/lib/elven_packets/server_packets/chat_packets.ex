198388c49a7f91881eded10718bac53a9b7c17e8
Fizo55 <hugomacees56000@gmail.com>
2023-08-29 10:17:58 +0200

    chore: add Worldie's packet but cleaned up

---
+++ apps/elven_packets/lib/elven_packets/server_packets/chat_packets.ex
@@ -0,0 +1,26 @@
+defmodule ElvenPackets.Server.ChatPackets do
+  @moduledoc """
+  TODO: ElvenPackets.Server.ChatPackets
+  """
+
+  use ElvenGard.Network.PacketSerializer
+
+  import ElvenPackets.Enums.ChatEnums, only: [color_type: 1]
+  import ElvenEnums.EntityEnums, only: [entity_type: 1]
+
+  alias ElvenPackets.Types.{NsEnum, NsInteger, NsString}
+
+  @serializable true
+  defpacket "bn", as: Bn do
+    field :id, NsInteger
+    field :message, NsString, escape: true
+  end
+
+  @serializable true
+  defpacket "say", as: Say do
+    field :entity_type, NsEnum, values: entity_type(:__enumerators__)
+    field :entity_id, NsInteger
+    field :color, NsEnum, values: color_type(:__enumerators__), default: :default
+    field :message, NsString
+  end
+end
