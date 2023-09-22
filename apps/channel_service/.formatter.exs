261c2b84ef21079e0e3df4b5483e354010b828bf
DarkyZ aka NotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2023-09-22 13:35:22 +0200

    :ambulance: Fix CI (2)

--- apps/channel_service/.formatter.exs
+++ apps/channel_service/.formatter.exs
@@ -1,5 +1,8 @@
 # Used by "mix format"
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  import_deps: [:elvengard_network]
+  import_deps: [:elvengard_network],
+  locals_without_parens: [
+    defcommand: 2
+  ]
 ]
