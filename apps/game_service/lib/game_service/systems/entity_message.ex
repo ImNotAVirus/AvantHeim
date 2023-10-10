737154d0c267caeaecf66fccc449ba3269aecc27
Fizo55 <hugomacees56000@gmail.com>
2023-10-10 20:01:03 +0200

    feat: add private chat

--- apps/game_service/lib/game_service/systems/entity_message.ex
+++ apps/game_service/lib/game_service/systems/entity_message.ex
@@ -37,4 +37,32 @@ defmodule GameService.EntityMessageSystem do
       GameService.System.map_event(event, position, [entity])
     end
   end
+
+  @impl true
+  def run(%EntityMessage{scope: :private, target_type: :player} = event, _context) do
+    # Creating our event message, to send map message
+    %EntityMessage{
+      entity_type: entity_type,
+      entity_id: entity_id,
+      message: message,
+      target_id: target_id
+    } = event
+
+    # In the GameService, Entity's id is a combination of it's type and it's id
+    ecs_id = GameService.real_entity_id(entity_type, entity_id)
+
+    endpoints =
+      P.EndpointComponent
+      |> Query.select(with: [{P.PlayerComponent, [{:==, :id, target_id}]}])
+      |> Query.all()
+
+    # Check if the Entity exists
+    with :ok <- Query.fetch_entity(ecs_id),
+         :ok do
+      # Finally, notify player with target_id
+      event = {:chat_message, entity_type, entity_id, message}
+
+      GameService.broadcast_to(event, endpoints)
+    end
+  end
 end
--- apps/game_service/test/game_service/systems/entity_message_system_test.exs
+++ apps/game_service/test/game_service/systems/entity_message_system_test.exs
@@ -49,5 +49,28 @@ defmodule GameService.EntityMessageSystemTest do
       # # We shouldn't receive an event
       refute_received {:chat_message, _, _, _}
     end
+
+    test "system event on entity writting in private message" do
+      # Register our process to receive message
+      ref = make_ref()
+      position = %E.PositionComponent{map_ref: ref}
+      endpoint = %P.EndpointComponent{pid: self()}
+      entity = spawn_player(components: [endpoint, position])
+      target_entity = spawn_player(components: [endpoint, position])
+
+      # Call our System with a EntityMessage event
+      event = %Evt.EntityMessage{
+        entity_type: GameService.entity_type(entity),
+        entity_id: GameService.entity_id(entity),
+        scope: :private,
+        message: "Best private message",
+        target_id: GameService.entity_id(target_entity)
+      }
+
+      _ = EntityMessageSystem.run(event, 0)
+
+      # # We shouldn't receive an event
+      refute_received {:chat_message, _, _, _}
+    end
   end
 end
